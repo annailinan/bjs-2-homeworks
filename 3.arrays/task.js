36c07e4e29b8202f10c58872c8591666a1ec9941
Anna Li <Kristusha3@yandex.ru>
2023-09-05 19:18:07 +0300

    Реализовано третье задание

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,22 @@
 function compareArrays(arr1, arr2) {
-  
+	if (arr1.length !== arr2.length) {
+		return false;
+	}
+
+	return arr1.every(function(element, index) {
+		return element === arr2[index];
+	})
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+	if (users.length === 0) {
+		return 0;
+	}
+	const user_filtres = users.filter(user => user.gender === gender);
+	if (user_filtres.length === 0) {
+		return 0;
+	}
+	const ages = user_filtres.map(user => user.age);
+	const avgAge = ages.reduce((sum, age) => sum + age, 0) / ages.length;
+	return avgAge;
 }
